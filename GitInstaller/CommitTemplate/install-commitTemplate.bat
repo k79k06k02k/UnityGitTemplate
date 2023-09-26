3ce0849a68054065d9c5e4cf52b9b80b9f02fcc3
Arkai <k79k06k02k@gmail.com>
2023-09-26 19:08:26 +0800

    Update install-commitTemplate.bat

--- GitInstaller/CommitTemplate/install-commitTemplate.bat
+++ GitInstaller/CommitTemplate/install-commitTemplate.bat
@@ -5,8 +5,8 @@ for %%a in ("%parent:~0,-1%") do set ParentPath=%%~dpa
 for %%a in ("%ParentPath:~0,-1%") do set ParentPath=%%~dpa
 set CurrentFath=%~dp0
 
-set From=%CurrentFath%commit_msg_template.txt
-set To=%ParentPath%.git\commit_msg_template.txt
+set From="%CurrentFath%commit_msg_template.txt"
+set To="%ParentPath%.git\commit_msg_template.txt"
 
 echo %From%
 echo %To%
